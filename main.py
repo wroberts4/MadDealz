64a1a004ac45cc45174e1f41c59bdcbf12dedea6
wroberts4 <38170479+wroberts4@users.noreply.github.com>
2019-06-12 22:28:23 -0700

    Add color

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home.html")
 def hello():
-    links = {'bool_link': render_template('link.html', href='bools.html', id='bool_link')}
+    links = {'bool_link': render_template('link.html', href='bools.html', id='bool_link', div='test')}
     return render_template('home.html', **links)
 
 
 @app.route("/bools.html")
 def bools():
-    links = {'home_link': render_template('link.html', href='home.html', id='home_link')}
+    links = {'home_link': render_template('link.html', href='home.html', id='home_link', div='body')}
     return render_template('bools.html', **links)
 
 

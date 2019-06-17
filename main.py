03300aadc61cb90949cb6db44ae70a2d0bd90559
wroberts4 <38170479+wroberts4@users.noreply.github.com>
2019-06-16 20:44:54 -0700

    Add table support

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home.html")
 def hello():
-    links = {'bool_link': render_template('link.html', href='bools.html', id='bool_link', div='test')}
-    return render_template('home.html', **links)
+    links = {'bool_link': render_template('link.html', href='bools.html', id='bool_link', div='text')}
+    tables = {'monday': render_template('table.html', id='monday', div='text',
+                                        data=[['Bar Name', 'Deal', 'Start Time', 'End Time'],
+                                              ['State Street Brats', '$1 Hamburgers', '9pm', '10pm']])}
+    return render_template('home.html', **links, **tables)
 
 
 @app.route("/bools.html")

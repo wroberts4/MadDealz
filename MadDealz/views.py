333230e6988e20c96535d6dcab9eeb7d52d70949
wroberts <wroberts4@wisc.edu>
2020-02-13 14:24:28 -0600

    Improve structure and style

--- MadDealz/views.py
+++ MadDealz/views.py
@@ -17,10 +17,11 @@ class Context(dict):
 def home(request):
     context = Context(request)
     # Add a link template.
-    context.render('the_bool_link', 'link.html', href='/the_bool.html', id='the_bool_link', div='text')
+    context.render('the_bool_link', 'links/link.html', href='/the_bool.html',
+                   id='the_bool_link', div='text', text='the_bool_link')
 
     # Temporary data.
-    data = {'State Street Brats': {'$1 Hamburgers': {'extra_info': '', 'days':
+    database = {'State Street Brats': {'$1 Hamburgers': {'extra_info': '', 'days':
         {'monday': {'start_time': '9pm', 'end_time': '10pm'},
          'tuesday': {'start_time': '9pm', 'end_time': '10pm'},
          'friday': {'start_time': '9pm', 'end_time': '10pm'}}},
@@ -33,19 +34,16 @@ def home(request):
                  'tuesday': {'start_time': '1pm', 'end_time': '2pm'},
                  'friday': {'start_time': '1pm', 'end_time': '2pm'}}}}}
 
-    # Add sorting code.
-    context.render('sort_db', 'sort.html', id='sort_db', div='text')
     # Add a table template for each day.
-    for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
-        context.render(day, 'table.html', id=day, div='text', data=data, info={'day': day, 'function': 'sort_by_day'})
+    context.render('table', 'tables/sorted_tables.html', div='text', database=database, function='sort_by_day')
 
     # Show home.html template.
-    return render(request, 'home.html', context)
+    return render(request, 'pages/home.html', context)
 
 
 def the_bool(request):
     context = Context(request)
     # Add a link template.
-    context.render('home_link', 'link.html', href='/home.html', id='home_link', div='body')
+    context.render('home_link', 'links/link.html', div='body', href='/home.html', id='home_link', text='home_link')
     # show the_bool.html template.
-    return render(request, 'the_bool.html', context)
+    return render(request, 'pages/the_bool.html', context)

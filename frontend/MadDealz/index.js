3e11588d2fcf845d10cb829f33edb18e5df2e53d
casce <wroberts4@wisc.edu>
2020-03-22 02:18:19 -0500

    Add basics for react-native-navigation

--- frontend/MadDealz/index.js
+++ frontend/MadDealz/index.js
@@ -5,5 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Navigation } from "react-native-navigation";
 
 AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});

02d8598607a97598e3b3b32be9ac31e4999d9958
NicPav1 <nicpav99@gmail.com>
2020-03-24 15:20:57 -0500

    created profile.js, home.js, and added them to the bottom tab

--- frontend/MadDealz/index.js
+++ frontend/MadDealz/index.js
@@ -9,12 +9,14 @@ import { Navigation } from "react-native-navigation";
 import Barpage from './barpage'; // Barpage screen
 import Home from  './home'; // Home screen
 import Profile from './profile'; // Profile screen
+import Favorites from './favorites'; // Favorites page
 
 AppRegistry.registerComponent(appName, () => App);
 Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
 Navigation.registerComponent('BarpageScreen', () => Barpage);
 Navigation.registerComponent('HomeScreen', () => Home);
 Navigation.registerComponent('ProfileScreen', () => Profile);
+Navigation.registerComponent('FavoritesScreen', () => Favorites);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
@@ -38,6 +40,16 @@ Navigation.events().registerAppLaunchedListener(() => {
               options: {
                 bottomTab: {
                   text: 'Profile'
+                }
+              }
+            }
+          },
+          {
+            component: {
+              name: "FavoritesScreen",
+              options: {
+                bottomTab: {
+                  text: 'Favorites'
                 }
               }
             }

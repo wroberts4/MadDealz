57ab09519de433977e95763efd003a0eb5ab8b1b
Chaly Xiong <gniox15@gmail.com>
2020-03-31 00:40:55 -0500

    filter with toggle switch

--- frontend/MadDealz/android/app/src/main/java/com/maddealz/MainActivity.java
+++ frontend/MadDealz/android/app/src/main/java/com/maddealz/MainActivity.java
@@ -1,8 +1,13 @@
 package com.maddealz;
 
 import com.reactnativenavigation.NavigationActivity;
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends NavigationActivity {
-
-  
+    @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity 
+  }
 }
--- frontend/MadDealz/ios/MadDealz/AppDelegate.m
+++ frontend/MadDealz/ios/MadDealz/AppDelegate.m
@@ -9,11 +9,17 @@
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
 
 #import <React/RCTBundleURLProvider.h>
+#import "RNBootSplash.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  rootViewController.view = rootView;
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView]; // <- initialization using the storyboard file name
+ 
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
   [ReactNativeNavigation bootstrapWithDelegate:self launchOptions:launchOptions];
   return YES;
 }

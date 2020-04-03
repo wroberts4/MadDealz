0bf79e61286bfb4e81472ab0641a321948a2e619
casce <wroberts4@wisc.edu>
2020-04-03 16:47:50 -0500

    Add console messages to get_user

--- backend/models/user.js
+++ backend/models/user.js
@@ -64,7 +64,7 @@ async function _get_user(username) {
 }
 
 export async function get_user(username) {
-  console.log(username);
+  console.log('TYPEOF USERNAME', typeof username);
   username = convert_to_object(username);
   if (!username) {
     return { status: 400, message: "Must specify a username" };
@@ -123,7 +123,7 @@ export async function user_login(user) {
 }
 
 export async function update_user(user) {
-  console.log('TYPEOF USER:', typeof user)
+  console.log(user)
   user = convert_to_object(user);
 // Note: we may want to get the _id from the username for future use?
   if (!user.username) {

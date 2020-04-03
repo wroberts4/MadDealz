c97f8d75dc910539e27473800576624267673fd1
casce <wroberts4@wisc.edu>
2020-04-03 15:40:46 -0500

    export convert_to_object

--- backend/utils/convert_to_object.js
+++ backend/utils/convert_to_object.js
@@ -1,6 +1,6 @@
 // This function recursively converts input into a javascript object, preserving
 // "falsy" objects.
-function convert_to_object(string) {
+export function convert_to_object(string) {
   // JSON.parse does not convert undefined string to undefined or NaN string to NaN.
   if (string === "undefined") {
     return undefined;

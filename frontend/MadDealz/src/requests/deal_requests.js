f076545c1b2810e8a904de2d77362b31ca36b014
casce <wroberts4@wisc.edu>
2020-04-04 14:00:25 -0500

    Connect review and deal requests

--- frontend/MadDealz/src/requests/deal_requests.js
+++ frontend/MadDealz/src/requests/deal_requests.js
@@ -9,16 +9,96 @@ const falsy_to_empty = require('../utils/requests').falsy_to_empty;
  *
  * @return {object} deal object containing name and address.
  */
-async function create_deal(name, address, ip, fetch_timeout){
+async function create_deal(info, bar, times, fetch_timeout) {
+    info = falsy_to_empty(info);
+    bar = falsy_to_empty(bar);
+    times = falsy_to_empty(times);
+    data = {'info': info, 'bar': bar, 'times': times};
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/deal/create';
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new deal object.
+    return (await response.json()).deal;
 };
 
-async function get_deal(id, ip, fetch_timeout) {
+async function get_deal(id, fetch_timeout, ip) {
+    id = falsy_to_empty(id);
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/deal?id=' + id;
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new deal object.
+    return (await response.json()).deal;
 };
 
-async function delete_deal(id, ip, fetch_timeout) {
+async function delete_deal(id, fetch_timeout, ip) {
+    id = falsy_to_empty(id);
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/deal/delete?id=' + id;
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new deal object.
+    return (await response.json()).message;
 };
 
-async function update_deal(deal, ip, fetch_timeout) {
+async function update_deal(info, bar, times, fetch_timeout, ip) {
+    info = falsy_to_empty(info);
+    bar = falsy_to_empty(bar);
+    times = falsy_to_empty(times);
+    data = {'info': info, 'bar': bar, 'times': times};
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/deal/update';
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new deal object.
+    return (await response.json()).message;
 };
 
 module.exports = {create_deal, get_deal, delete_deal, update_deal};
--- frontend/MadDealz/src/requests/review_requests.js
+++ frontend/MadDealz/src/requests/review_requests.js
@@ -1,16 +1,98 @@
 const fetchWithTimeout = require('../utils/requests').fetchWithTimeout;
 const falsy_to_empty = require('../utils/requests').falsy_to_empty;
 
-async function create_review(review, ip, fetch_timeout) {
+async function create_review(content, bar, score, user, fetch_timeout, ip) {
+    content = falsy_to_empty(content);
+    bar = falsy_to_empty(bar);
+    score = falsy_to_empty(score);
+    user = falsy_to_empty(user);
+    data = {'content': content, 'bar': bar, 'score': score, 'user': user};
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/review/create';
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new review object.
+    return (await response.json()).review;
 };
 
-async function get_review(id, ip, fetch_timeout) {
+async function get_review(id, fetch_timeout, ip) {
+    id = falsy_to_empty(id);
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/review?id=' + id;
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new review object.
+    return (await response.json()).review;
 };
 
-async function delete_review(id, ip, fetch_timeout) {
+async function delete_review(id, fetch_timeout, ip) {
+    id = falsy_to_empty(id);
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/review/delete?id=';
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new review object.
+    return (await response.json()).message;
 };
 
-async function update_review(review, ip, fetch_timeout) {
+async function update_review(content, bar, score, fetch_timeout, ip) {
+    content = falsy_to_empty(content);
+    bar = falsy_to_empty(bar);
+    score = falsy_to_empty(score);
+    user = falsy_to_empty(user);
+    data = {'content': content, 'bar': bar, 'score': score};
+    ip = ip ? ip : 'https://api.maddealz.software';
+    let url = ip + '/review/update';
+    const response = await fetchWithTimeout(
+        url,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        },
+        fetch_timeout
+    );
+    if (response.status != 200) {
+        throw (await response.json()).message;
+    }
+    // Returns a new review object.
+    return (await response.json()).message;
 };
 
 module.exports = {create_review, get_review, delete_review, update_review};

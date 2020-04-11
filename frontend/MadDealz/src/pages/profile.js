e442f740c66f7e79eeea97ce151320ab761b4090
Benedict Donnay <Benedictluc@gmail.com>
2020-04-11 14:08:43 -0500

    Start of Profile Page

--- frontend/MadDealz/src/pages/profile.js
+++ frontend/MadDealz/src/pages/profile.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
+import {Card, Image, Button, Icon, Header, Avatar} from 'react-native-elements';
 
 import {goToLogin, goToChangePwd} from '../../navigation';
 
@@ -13,17 +22,80 @@ export default class Profile extends Component {
   };
 
   render() {
+    const {userId} = this.props;
+
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={this.loginPage}>
-          <Text style={{fontWeight: '500', color: '#222'}}>Logout</Text>
-        </TouchableOpacity>
+        <Header
+          containerStyle={{backgroundColor: '#990000'}}
+          leftComponent={
+            <Button
+              icon={<Icon name="arrow-back" />}
+              onPress={this.tabsPage}
+              buttonStyle={{backgroundColor: '#990000'}}
+            />
+          }
+          centerComponent={{text: userId}}
+        />
+        <ScrollView style={styles.scroll}>
+          
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingTop: 5,
+            }}>
+            <Avatar
+              rounded
+              size = {200}
+              source={require('../../assets/Husky.jpg')}
+              resizeMode="contain"
+              showEditButton
+            />
+            <Text style={{color: 'white'}}>{userId}</Text>
+        </View>
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputTitle}>Email Address</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              returnKeyType="next"
+              autoCompleteType="email"
+              textContentType="emailAddress"
+              keyboardType="email-address"></TextInput>
+          </View>
 
-        <TouchableOpacity style={styles.button} onPress={this.changePwdPage}>
-          <Text style={{fontWeight: '500', color: '#222'}}>
-            Change Password
-          </Text>
-        </TouchableOpacity>
+          <View style={{marginTop: 32}}>
+            <Text style={styles.inputTitle}>Old Password</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              autoCapitalize="none"
+              returnKeyType="done"></TextInput>
+          </View>
+
+          <View style={{marginTop: 32}}>
+            <Text style={styles.inputTitle}>New Password</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              autoCapitalize="none"
+              returnKeyType="done"></TextInput>
+          </View>
+
+        </View>
+
+          <TouchableOpacity style={styles.button} onPress={this.loginPage}>
+            <Text style={{fontWeight: '500', color: '#222'}}>Logout</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={this.changePwdPage}>
+            <Text style={{fontWeight: '500', color: '#222'}}>
+              Change Password
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     );
   }
@@ -36,11 +108,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#000',
   },
+  form: {
+    marginTop: 28,
+    marginBottom: 48,
+    marginHorizontal: 30,
+    height: 200,
+    width: Dimensions.get('screen').width - 40,
+  },
+  inputTitle: {
+    color: '#ccc',
+    fontSize: 10,
+    textTransform: 'uppercase',
+  },
+  input: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 15,
+    color: '#eee',
+  },
   button: {
     marginHorizontal: 30,
     backgroundColor: '#f55',
     borderRadius: 4,
-    height: 52,
+    height: 32,
     alignItems: 'center',
     justifyContent: 'center',
   },

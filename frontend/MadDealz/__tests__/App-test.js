2f1ad54d2c8faf6da87c134d1d4c7a35d545b67a
NicPav1 <nicpav99@gmail.com>
2020-04-12 19:11:33 -0500

    added snapshot test for contact page

--- frontend/MadDealz/__tests__/App-test.js
+++ frontend/MadDealz/__tests__/App-test.js
@@ -6,6 +6,7 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 import Barpage from '../src/pages/barpage';
+import Contact from '../src/pages/contact';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -13,5 +14,10 @@ import renderer from 'react-test-renderer';
 // Tests barpage snapshot
 test('renders correctly', () => {
     const tree = renderer.create(<Barpage />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly', () => {
+    const tree = renderer.create(<Contact />).toJSON();
     expect(tree).toMatchSnapshot();
 });
